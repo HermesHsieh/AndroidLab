d944a4efc7b4ac11c9cb8c1d2103c97bd9df8035
Hermes <hermes.hsien@gmail.com>
2018-12-31 17:25:45 +0800

    Create BottomNavigationActivity

--- app/src/main/kotlin/android/lab/app/activity/main/MainActivity.kt
+++ app/src/main/kotlin/android/lab/app/activity/main/MainActivity.kt
@@ -1,13 +1,19 @@
 package android.lab.app.activity.main
 
 import android.lab.app.R
+import android.lab.app.activity.bottomnavigation.BottomNavigationActivity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_bottom_navigation.setOnClickListener {
+            startActivity<BottomNavigationActivity>()
+        }
     }
 }
